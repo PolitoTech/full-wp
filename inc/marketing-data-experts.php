96530c278e6d5a48f43c7d93dc8125e1baabd36b
Polito Technologies <info@politotech.com>
2015-06-19 12:15:24 -0400

    Update marketing-data-experts.php

--- inc/marketing-data-experts.php
+++ inc/marketing-data-experts.php
@@ -5,9 +5,9 @@
           <div class="col-sm-6">
             <article>
               <h4><i class="fa fa-rss"></i> Blog Posts: </h4>
-              <p>Best Practices in Designing a Lead Lifecycle in Salesforce</p>
-              <p>Turbo Charge Your Analytics in Salesforce</p>
-              <p>Optimize Your Marketing Mix in Salesforce</p>
+              <a href="/full-wp/how-to-get-sales-and-marketing-in-alignment/">How To Get Sales and Marketing in Alignment</a>
+              <a href="/full-wp/why-executives-need-data-driven-insights-from-marketing/">Why Executives Need Data Driven Insights From Marketing</a>
+              <a href="/how-to-get-key-marketing-metrics-from-your-crm-system/">How To Get Key Marketing Metrics From Your CRM System</a>
             </article>
           </div>
           <div class="col-sm-6">
